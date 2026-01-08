165bb5839932cd5baa3c6a4993c8243eb22c743c
Dudzsantana <dudu_nunes_159@hotmail.co.jp>
2026-01-08 15:00:47 -0300

    Add insecure code examples for testing in app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+token_projeto = os.getenv('TESTE_SECRET_KEY')
+
+# Dica de DevSecOps: É sempre bom verificar se a chave foi encaontrada
+if not token_projeto:
+    print("⚠️ Aviso: A variável TESTE_SECRET_KEY não foi configuradaaa!")
+
+
+
+def minha_funcao():
+  x = 10
+  return x
+
+# --- CÓDIGO INSEGURO PARA TESTE ---
+
+# 1. Segredo exposto (Hardcoded Password)
+DB_PASSWORD = "Admin_Password_123!" 
+
+# 2. Comando de sistema inseguro (Potencial Injeção de Comando)
+import os
+usuario_input = "ls" # Simula um dado que viria de um utilizador
+os.system("echo Executando comando: " + usuario_input)
+
+# 3. Uso de Eval (Execução de código arbitrário)
+comando_perigoso = "2 + 2"
+resultado = eval(comando_perigoso)
+
+import sqlite3
+
+def buscar_utilizador(id_utilizador):
+    conn = sqlite3.connect('base_de_dados.db')
+    cursor = conn.cursor()
+    # ESTA LINHA É UM PECADO CAPITAL EM SEGURANÇA:
+    query = "SELECT * FROM users WHERE id = " + id_utilizador 
+    cursor.execute(query)
+    return cursor.fetchone()
