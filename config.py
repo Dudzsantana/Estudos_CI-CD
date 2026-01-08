088f7d06f7b157a4d0823b64088e42a159e39f1d
Dudzsantana <dudu_nunes_159@hotmail.co.jp>
2026-01-08 01:16:07 -0300

    linty

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
+import os
+import sys # Importado mas nunca usado
 
 token_projeto = os.getenv('TESTE_SECRET_KEY')
 
 # Dica de DevSecOps: É sempre bom verificar se a chave foi encaontrada
 if not token_projeto:
     print("⚠️ Aviso: A variável TESTE_SECRET_KEY não foi configuradaaa!")
+
+
+
+def minha_funcao():
+  x = 10
+    return x # Erro de indentação aqui
