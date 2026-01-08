66d4fe2c34524df02a7bb1b598511ac99d14f785
Dudzsantana <dudu_nunes_159@hotmail.co.jp>
2026-01-07 23:49:16 -0300

    adicionando variavel de ambiente

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-# Um token do GitHub (formato ghp_ seguido de 36 caracteres alfanuméricos)
-admin_token = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+import os
+
+# O Python busca a variável no ambiente do GitHub
+api_key = os.getenv('MINHA_CHAVE_SECRETA')
+
+if api_key:
+    print("Conectado com sucesso!")
+else:
+    print("Erro: Chave não encontrada no ambiente.")
