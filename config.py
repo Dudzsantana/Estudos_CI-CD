7c423005e837a2cc88f4c3c484e23c7f42490751
Dudzsantana <dudu_nunes_159@hotmail.co.jp>
2026-01-08 00:39:11 -0300

    teste

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
-# Em vez de 'ghp_123...', pedimos ao sistema a variável 'GITHUB_TOKEN'
 token_projeto = os.getenv('TESTE_SECRET_KEY')
 
-# Dica de DevSecOps: É sempre bom verificar se a chave foi encontrada
+# Dica de DevSecOps: É sempre bom verificar se a chave foi encaontrada
 if not token_projeto:
-    print("⚠️ Aviso: A variável TESTE_SECRET_KEY não foi configurada!")
+    print("⚠️ Aviso: A variável TESTE_SECRET_KEY não foi configuradaaa!")
